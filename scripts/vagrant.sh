76c5f94ea3e6fc5e96119baa25c53dc7bd9544d0
Iku Iwasa <iku.iwasa@gmail.com>
2023-03-06 22:57:45 +0900

    Add nameserver 1.1.1.1 as DNS lookup fails in some reason

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -5,5 +5,7 @@ set -e
 mkdir "$HOME/.ssh"
 chmod 700 "$HOME/.ssh"
 
+echo 'nameserver 1.1.1.1' | su root -c '/sbin/resolvconf -a network'
+
 ftp https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
 mv vagrant.pub "$HOME/.ssh/authorized_keys"
